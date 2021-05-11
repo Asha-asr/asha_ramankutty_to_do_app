8082e0c495160c3fcce29062f1782b1ce87e142c
Asha-asr <asharamankutty20@gmail.com>
2021-05-11 13:15:29 +0530

    Second:Changed

---
+++ to-do-list.js
@@ -0,0 +1,12 @@
+let user_name = document.getElementById("username");
+let password = document.getElementById("password");
+
+function validate(){
+    if(user_name.value == "" || password.value == ""){
+        alert("Please enter all fields");
+    }
+
+    else{
+
+    }
+}
